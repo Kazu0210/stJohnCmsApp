f04fd7b85fef1e8dca84f58fdcf4ad5d3563c23b
Kazu0210 <clffrdkyl@gmail.com>
2025-10-17 10:56:10 +0800

    fixed bug on populating payments table

--- cmsApp/frontend/admin/adminFinancial/adminFinancial.php
+++ cmsApp/frontend/admin/adminFinancial/adminFinancial.php
@@ -22,6 +22,10 @@ $userRole = getCurrentUserRole();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js"></script>
+    <!-- jQuery and DataTables CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
     <link rel="stylesheet" href="adminFinancial.css">
     
     <style>
@@ -295,5 +299,6 @@ $userRole = getCurrentUserRole();
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="adminFinancial.js"></script>
+    <script src="fetchPayments.js"></script>
 </body>
 </html>
